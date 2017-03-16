e659d61dbc300cf2d83f0181d3247068cf83d3c5
alexis <lex@livetotry.com>
2017-03-16 10:40:12 -0400

    added parsing of odds from cbs sports

--- init_bracket.py
+++ init_bracket.py
@@ -1,4 +1,7 @@
 import csv 
+from bs4 import BeautifulSoup
+import urllib
+import re
 
 class Bracket(object):
     def __init__(self, starting_teams):
@@ -54,21 +57,59 @@ def fill_bracket(matchups):
 def pick_winner(matchup):
     team1 = matchup.team1
     team2 = matchup.team2
+    
     if (vegas_line(team1) == vegas_line(team2)):
         # todo: change this to a random selection between the two
         return team1
     elif (vegas_line(team1) > vegas_line(team2)): return team1
     else: return team2
     
-def vegas_line(team):
-    # to-do: beautiful soup to parse http://www.vegasinsider.com/college-basketball/odds/las-vegas/
-    return 1
+def build_odds():
+    url = "http://www.cbssports.com/college-basketball/news/march-madness-2017-point-spreads-lines-odds-released-for-ncaa-tournament-games/"
+    r = urllib.urlopen(url).read()
+    soup = BeautifulSoup(r)
+    strong_tags = soup.select("strong") 
+    for t in strong_tags:
+        s = str(t.contents)
+        if ("day," in s):
+            table = t.parent.next_sibling
+            tds = table.select("tbody tr > td:nth-of-type(3)")
+            for td in tds:
+                if (td.has_attr('data-sheets-value')):
+                    odds = td['data-sheets-value']                    
+                    team_and_odds = re.search('.*:.*:\"(.*) (-.*)\"', odds, re.DOTALL)
+                    if team_and_odds:
+                        team = team_and_odds.group(1)
+                        team = team.replace(".", "")
+                        print("team: {0}".format(team))
+                        odds = team_and_odds.group(2)
+                        print("odds: {0}".format(odds))
+                    # now grab the teams from the cvs, and add the odds to it
     
+def vegas_line(team):
+    # format:
+    
+    # http://www.vegasinsider.com/college-basketball/matchups/matchups.cfm/date/MM-DD-YY
+    # to-do: parse out the odds
+#    print(soup.prettify())
+    return 1
+
+def team_record_weight(team):
+    return 10
+    
+def weight(team):
+    return random.randint(1,101)
+     #return (vegas_line(team) * 0.3) +  (record_team_weight(team) * 0.3) + (foo(team) * 0.4)
+     
 def main():
     starting_teams = parse_teams('data/first-four-teams.csv')
     matchups = build_matchups(starting_teams)
     for m in matchups:
         print("Matchup: {0} vs {1} - {2}".format(m.team1.name,m.team2.name,m.date))
+        winner = pick_winner(m)
+        print("Predict {0} will win".format(winner.name))
+    build_odds()
+    
     # to-do: loop through matches and call pick_winner on each    
     # to-do: loop through the initial data, and fill in with specific stats
 
